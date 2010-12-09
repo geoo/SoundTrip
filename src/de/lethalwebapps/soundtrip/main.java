41d0da9768aee2e4253c0bb26d90bb05b877158f
geoo <geo@nukular-pictures.de>
2010-12-09 01:53:10 +0100

    Gallery eingebaut

--- src/de/lethalwebapps/soundtrip/main.java
+++ src/de/lethalwebapps/soundtrip/main.java
@@ -1,9 +1,17 @@
 package de.lethalwebapps.soundtrip;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -12,6 +20,59 @@ public class main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
        
+        Gallery g = (Gallery) findViewById(R.id.GalleryMitte);
+        g.setAdapter(new ImageAdapter(this));
+
+        g.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Toast.makeText(main.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         
+    }
+    public class ImageAdapter extends BaseAdapter {
+        int mGalleryItemBackground;
+        private Context mContext;
+
+        private Integer[] mImageIds = {
+                R.drawable.sample_1,
+                R.drawable.sample_2,
+                R.drawable.sample_3,
+                R.drawable.sample_4,
+                R.drawable.sample_5,
+                R.drawable.sample_6,
+                R.drawable.sample_7
+        };
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+            TypedArray a = obtainStyledAttributes(R.styleable.Gallery);
+            mGalleryItemBackground = a.getResourceId(R.styleable.Gallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+
+        public int getCount() {
+            return mImageIds.length;
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView i = new ImageView(mContext);
+
+            i.setImageResource(mImageIds[position]);
+            i.setLayoutParams(new Gallery.LayoutParams(500, 300));
+            i.setScaleType(ImageView.ScaleType.FIT_XY);
+            i.setBackgroundResource(mGalleryItemBackground);
+
+            return i;
+        }
     }
 }
